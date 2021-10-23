012937a0cc95c441896d94da07fde9b3ca9279b2
Murilo Vicente <murilovicent@gmail.com>
2021-10-23 16:12:52 -0300

    Primeiro commit

---
+++ exercicios/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+import Pai from './components/direta/Pai'
+// import Contador from './components/Contador'
+// import Botao from './components/Botao'
+// import Titulo from './components/Titulo'
+// import Aleatorio from './components/Aleatorio'
+// import MinMax from './components/MinMax'
+// import CompPadrao, { Comp1, Comp2 } from './components/Multi'
+// import Primeiro from './components/Primeiro'
+
+export default () => (
+    <View style={style.App}>
+        <Pai />
+        {/* 
+        <Contador inicial={0} passo={5}/>
+        <Contador />
+        <Botao  />
+        <Titulo principal='Cadastro Produto'
+                secundario='Tela de Cadastro do Produto' />
+        <Aleatorio min={1} max={60} />
+        <MinMax min='3' max='20' />
+        <MinMax min='1' max='94' />
+        <CompPadrao />
+        <Comp1 />
+        <Comp2 />
+        <Primeiro />  */}
+    </View>
+)
+
+const style = StyleSheet.create({
+    App: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20
+    }
+})
